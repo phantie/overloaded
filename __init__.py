8a07e0ad7aa43ddc0740b2d375c05e60dc50bafa
phantie <funtom1605@gmail.com>
2020-09-02 23:07:06 +0300

    cleanup

--- __init__.py
+++ __init__.py
@@ -1,84 +1,28 @@
 from functools import wraps
-import inspect
-
-def get_default_args(func):
-    signature = inspect.signature(func)
-    return {
-        k: v.default
-        for k, v in signature.parameters.items()
-        if v.default is not inspect.Parameter.empty
-    }
-
-class DetailedFunction:
-    def __new__(cls, f):
-        assert callable(f)
-        return super().__new__(cls)
-
-    def __init__(self, f):
-        def get_unsetargs(args, defaults):
-            for i, el in enumerate(args):
-                if el in defaults:
-                    edge = i
-                    break
-            else:
-                edge = len(args)
-
-            args = args[:edge]
-            return args
-
-        self.f = f
-        code = f.__code__
-
-        self.varnames = code.co_varnames
-
-        self.argcount = code.co_argcount # a, /, b, *, c  NOTE: includes normail and positional-only args 
-                                         # ^     ^     ^
-        self.posonlyargcount = code.co_posonlyargcount # a, /, b, *, c
-                                                       # ^
-        self.kwonlyargcount = code.co_kwonlyargcount # a, /, b, *, c
-                                                     #             ^
-        self.defaultargs = get_default_args(f)
-        self.unsetargs = get_unsetargs(self.varnames, self.defaultargs)
-
-    def display(self):
-        print(f"name: {self.f.__name__}")
-        print(f"{self.varnames=}"[5:])
-        print(f"{self.argcount=}"[5:])
-        print(f"{self.posonlyargcount=}"[5:])
-        print(f"{self.kwonlyargcount=}"[5:])
-        print(f"{self.defaultargs=}"[5:])
-        print(f"{self.unsetargs=}"[5:])
-
     
 class FIterator:
 
     def __init__(self):
         self._store = []
-        self._idx = -1
 
     def append(self, f):
         self._store.append(f)
 
     def __call__(self, *args, **kwargs):
-        while 1:
-            self._idx += 1
+        idx = -1
+        while True:
+            idx += 1
 
             try:
-                result = self._store[self._idx](*args, **kwargs)
-                self.reset()
+                result = self._store[idx](*args, **kwargs)
                 return result
             except IndexError:
                 raise TypeError("Function(s) exist(s) but with a different signature.")
-            except (TypeError, NameError) as e:
-                if 'missing' in str(e) or 'takes' in str(e) or 'not defined' in str(e) or 'unexpected' in str(e) or 'multiple' in str(e):
+            except (TypeError, NameError) as e:                
+                if any(string in str(e) for string in ('missing', 'takes', 'not defined', 'unexpected', 'multiple')):
                     continue
                 else:
                     raise e
-            else:
-                break
-
-    def reset(self):
-        self._idx = -1
 
     def __len__(self):
         return self._store.__len__()
@@ -86,20 +30,13 @@ class FIterator:
 class defaultnamespace:
     def __init__(self, _type):
         self._type = _type
-        self.attrs = []
-
 
     def __getattribute__(self, name):
         try:
             return object.__getattribute__(self, name)
         except AttributeError:
-            object.__getattribute__(object.__getattribute__(self, 'attrs'), 'append')(name)
             self.__setattr__(name, self._type())
             return object.__getattribute__(self, name)
-
-    def display(self):
-        for el in self.attrs:
-            print(el, '=', len(getattr(self, el)))
 
 class Overloader:
     store = defaultnamespace(FIterator)
@@ -116,9 +53,7 @@ class Overloader:
         return object.__getattribute__(self, name)
 
 
-
 overloaded = Overloader()
-
 
 @overloaded
 def foo(a, b): 
@@ -144,7 +79,6 @@ def bar(a, b):
 def bar(a, b, c):
     print(a * b * c, 'sup')
 
-# overloaded.store.display()
 
 overloaded.foo(100, 10)
 overloaded.foo(1, 2, 3)
