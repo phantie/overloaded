8c342a1c2559660849c50b689eedc9278039e01d
phantie <funtom1605@gmail.com>
2020-09-06 05:15:05 +0300

    1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from os.path import join, dirname
 
 setup(
     name = 'overloaded',
-    version = 0.2,
+    version = 1.0,
     packages = find_packages(),
     long_description = open(join(dirname(__file__), 'README.md')).read(),
     install_requires=[
